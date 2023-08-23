beb9e9caaf330f5c6bee69c9af482907c74ccea6
jay841224 <j1360fy6416nb@gmail.com>
2023-08-23 17:05:14 +0800

    finish2

--- Budget/src/main/java/BudgetService.java
+++ Budget/src/main/java/BudgetService.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
@@ -27,25 +28,27 @@ public class BudgetService {
             return yearMonth.compareTo(startYearMonth) >= 0 && yearMonth.compareTo(endYearMonth) <= 0;
         }).map(budget -> {
             YearMonth yearMonth = YearMonth.parse(budget.getYearMonth(), DateTimeFormatter.ofPattern("yyyyMM"));
-            int dayOfMonth = yearMonth.atEndOfMonth().getDayOfMonth();
-            BigDecimal dailyAmount = budget.getAmount().divide(new BigDecimal(dayOfMonth));
 
             if (!yearMonth.equals(startYearMonth) && !yearMonth.equals(endYearMonth)) {
                 return budget.getAmount();
             }
 
+            int dayOfMonth = yearMonth.atEndOfMonth().getDayOfMonth();
+            BigDecimal dailyAmount = budget.getAmount().divide(new BigDecimal(dayOfMonth), 2, RoundingMode.HALF_UP);
+
             if (yearMonth.equals(startYearMonth)) {
-                BigDecimal bigDecimal;
+                BigDecimal days;
+
                 if (startYearMonth.equals(endYearMonth)) {
-                    bigDecimal = new BigDecimal(end.getDayOfMonth() - start.getDayOfMonth() + 1);
+                    days = new BigDecimal(end.getDayOfMonth() - start.getDayOfMonth() + 1);
                 } else {
-                    bigDecimal = new BigDecimal(dayOfMonth - start.getDayOfMonth() + 1);
+                    days = new BigDecimal(dayOfMonth - start.getDayOfMonth() + 1);
                 }
-                return dailyAmount.multiply(bigDecimal);
+
+                return dailyAmount.multiply(days);
             }
 
             return dailyAmount.multiply(new BigDecimal(end.getDayOfMonth()));
-
         }).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
