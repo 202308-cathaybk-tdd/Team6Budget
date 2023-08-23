de5d6b38d05406c985e29dd4913b5c1e5ec42799
Joey Chen <jay.hatelove@gmail.com>
2023-08-23 22:30:52 +0800

    use budgetList directly, instead of filter budgetList by period, but test fail

--- Budget/src/main/java/budget/BudgetService.java
+++ Budget/src/main/java/budget/BudgetService.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class BudgetService {
 
@@ -24,9 +25,10 @@ public class BudgetService {
         YearMonth startYearMonth = YearMonth.from(start);
         YearMonth endYearMonth = YearMonth.from(end);
 
-        return budgetList.stream().filter(budget -> {
-            YearMonth yearMonth = YearMonth.parse(budget.getYearMonth(), DateTimeFormatter.ofPattern("yyyyMM"));
-            return yearMonth.compareTo(startYearMonth) >= 0 && yearMonth.compareTo(endYearMonth) <= 0;
-        }).map(budget -> budget.overlappingAmount(new Period(start, end))).reduce(BigDecimal.ZERO, BigDecimal::add);
+//        Stream<Budget> budgets = budgetList.stream().filter(budget -> {
+//            YearMonth yearMonth = YearMonth.parse(budget.getYearMonth(), DateTimeFormatter.ofPattern("yyyyMM"));
+//            return yearMonth.compareTo(startYearMonth) >= 0 && yearMonth.compareTo(endYearMonth) <= 0;
+//        });
+        return budgetList.stream().map(budget -> budget.overlappingAmount(new Period(start, end))).reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
