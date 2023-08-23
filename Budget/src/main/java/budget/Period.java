8b066658b3ef4f7b99b61625daf942d2d51f77cc
Joey Chen <jay.hatelove@gmail.com>
2023-08-23 22:23:37 +0800

    create a variable: another from budget.firstDay() and budget.lastDay()

--- Budget/src/main/java/budget/Period.java
+++ Budget/src/main/java/budget/Period.java
@@ -6,6 +6,7 @@ import static java.time.temporal.ChronoUnit.DAYS;
 
 public record Period(LocalDate start, LocalDate end) {
     public long overlappingDays(Budget budget) {
+        Period another = new Period(budget.firstDay(), budget.lastDay());
         LocalDate firstDay = budget.firstDay();
         LocalDate lastDay = budget.lastDay();
         LocalDate overlappingStart = start().isAfter(firstDay)
